93a3e6042c4ad209b18bb5f574e7fb8b16480d56
Charankumdale <ckumdale27051999@gmail.com>
2022-11-12 08:26:55 +0530

    Added

---
+++ src/main/java/com/example/WebDevProjApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDevProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDevProjApplication.class, args);
+	}
+
+}
